8471a3071b715d3da6cb44773a3c4b3bf987091a
Андрей Волкодав <avolkodav@dcalmata.KTort.local>
2025-07-29 15:26:00 +0500

    fix: use environment variables for config

--- Desktop/TGBOTKULIKOV/config.py
+++ Desktop/TGBOTKULIKOV/config.py
@@ -1,3 +1,5 @@
+import os
+
 TOKEN = os.environ.get("TOKEN")
 ADMIN_ID = os.environ.get("ADMIN_ID")
-SPREADSHEET_NAME = os.environ.get("DAAMI_MK_Записи")
+SPREADSHEET_NAME = os.environ.get("SPREADSHEET_NAME")
