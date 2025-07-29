3e0765ecb114f0747e98a45e8367e037baa24e2e
Андрей Волкодав <avolkodav@dcalmata.KTort.local>
2025-07-29 15:35:31 +0500

    fix: correct env var names in config

--- Desktop/TGBOTKULIKOV/config.py
+++ Desktop/TGBOTKULIKOV/config.py
@@ -1,5 +1,5 @@
 import os
 
-TOKEN = os.environ.get("8001755667:AAF9IWZnDh3KAFUl5e_WOzkmDCUn-UHWEJc")
-ADMIN_ID = os.environ.get("980656033")
-SPREADSHEET_NAME = os.environ.get("DAAMI_MK_Записи")
+TOKEN = os.environ.get("TOKEN")
+ADMIN_ID = os.environ.get("ADMIN_ID")
+SPREADSHEET_NAME = os.environ.get("SPREADSHEET_NAME")
